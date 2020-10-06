27033fed568fef5210654653d10aa08b4e3fa846
ehlvi <ehlvira-andersen@yandex.ru>
2020-10-06 08:00:05 +0300

    TSK-1 add Film and FilmService

--- src/main/java/logic/App.java
+++ src/main/java/logic/App.java
@@ -1,7 +1,9 @@
 package logic;
 
 import domain.Actor;
+import domain.Film;
 import logic.service.ActorService;
+import logic.service.FilmService;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,15 +13,28 @@ public class App {
     public static void main(String[] args) {
 
         ActorService actorService = new ActorService();
+        FilmService filmService = new FilmService();
 
         List<Actor> allActors = actorService.getAllActors();
+        List<Film> allFilms = filmService.getAllFilms();
 
         System.out.println("Print List of results");
         for (Actor actor : allActors) {
             System.out.println(actor);
         }
+//        вывод столбиком построчно
+
 
         System.out.println("Another way to print List of data");
         System.out.println(Arrays.toString(allActors.toArray()));
+//        вывод через одну строчку всего списка
+
+        System.out.println("Print List of results");
+        for (Film film : allFilms) {
+            System.out.println(film);
+        }
+
+        System.out.println("Another way to print List of data");
+        System.out.println(Arrays.toString(allFilms.toArray()));
     }
 }
